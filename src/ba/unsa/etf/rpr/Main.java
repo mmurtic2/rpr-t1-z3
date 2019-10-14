f1fa87ad508144d67b55bd66228ce6b0c0a962e4
student <mmurtic2@etf.unsa.ba>
2019-10-14 14:30:53 +0200

    fg

---
+++ src/ba/unsa/etf/rpr/Main.java
@@ -0,0 +1,24 @@
+package ba.unsa.etf.rpr;
+
+
+
+
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        Sat s = new Sat(1,1,1);
+
+        s.Ispisi();
+        s.Sljedeci();
+        s.Ispisi();
+        s.PomjeriZa(-48);
+        s.Ispisi();
+        s.Postavi(0,0,0);
+        s.Ispisi();
+
+    }
+}
+
+
